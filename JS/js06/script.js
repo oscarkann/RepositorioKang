8843e97e14c212db1b6e079755ade89d390e8804
oscarkann <oscarkang97@gmail.com>
2023-02-20 10:29:12 -0600

    ejercicios extra

--- JS/js06/script.js
+++ JS/js06/script.js
@@ -102,3 +102,90 @@ var botonDivision = document.getElementById("botonDivision");
 
 // AQUI VA A APARECER EL RESULTADO
 var resultado = document.getElementById("resultado");
+
+//CONSTRUIMOS LAS FUNCIONES DE NUESTRA CALCULADORA
+
+function suma(){
+    let valor1 = parseInt(input1.value); //numero
+    let valor2 = parseInt(input2.value); //numero
+    let suma = valor1 + valor2; //calculo la suma
+    resultado.innerHTML = suma; //pongo el resultado en su lugar
+}
+
+function resta(){
+    let valor1 = parseInt(input1.value); //numero
+    let valor2 = parseInt(input2.value); //numero
+    let resta = valor1 - valor2; //calculo la suma
+    resultado.innerHTML = resta; //pongo el resultado en su lugar
+}
+
+function multiplicacion(){
+    let valor1 = parseInt(input1.value); //numero
+    let valor2 = parseInt(input2.value); //numero
+    let multiplicacion = valor1 * valor2; //calculo la suma
+    resultado.innerHTML = multiplicacion; //pongo el resultado en su lugar
+}
+
+function division(){
+    let valor1 = parseInt(input1.value); //numero
+    let valor2 = parseInt(input2.value); //numero
+    let division = valor1 / valor2; //calculo la suma
+    resultado.innerHTML = division; //pongo el resultado en su lugar
+}
+
+
+/*
+COMO SE CREA UN EVENTO (addEventlistener)
+
+    - node.addEventListener ("evento a escuchar", lo que quiero que haga cuando se escuche)
+
+        - node (nodo donde aterrizo el evento)
+        - addEventListener (palabra reservada para usar el evento)
+        - evento a escuchar (click, mouseover, etc)
+        - Lo que quiero que haga (la llamada de la funcion)
+
+*/
+
+botonSuma.addEventListener("click", suma);
+botonResta.addEventListener("click", resta);
+botonMultiplicacion.addEventListener("click", multiplicacion);
+botonDivision.addEventListener("click", division);
+/*
+
+Manipulacion del DOM
+
+
+    - Metodos para acceder a elementos 
+
+        - document.getElementById
+        - document.getElementsByTagName
+        - document.getElementosByClassName
+
+
+    - Metodos para crear elementos  
+
+        - document.createElement(etiqueta)
+        - document.createTextNode(texto) - Investigar
+
+
+    - Metodos para insertar elementos
+
+        - parentElement.append
+        - parentElement.insertBefore
+        - parentElement.insertAdjacentElement
+
+
+    - Metodos para modificar elementos
+
+        - node.outerHTML (leer o referenciar el elemento)
+        - node.innerHTML (modificar el elemento)
+
+
+*/
+
+//Paso 1: Definir con que voy a interactuar (almacenado)
+const textoAModificar = document.querySelector("#h1")
+
+function cambiarColor(color){
+    textoAModificar.style.color = color;
+}
