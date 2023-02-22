df81af6f388c4af325adff73653d56481d5b9dbb
oscarkann <oscarkang97@gmail.com>
2023-02-22 10:59:45 -0600

    js09 parrot

--- JS/js09/index.js
+++ JS/js09/index.js
@@ -4,4 +4,4 @@ const parrotSay = require('parrotsay-api')
 
 parrotSay('La Pepo siempre viene aquí queriendo llamar la atención Mm, no sé cuál sea la fijación que tiene. No sé cuál sea la fijación que tiene')
     .then(console.log)
-    .catch(console.error)
+    .catch(console.error)git 
