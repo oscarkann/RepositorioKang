c29750ad324b6469628f2130204056bd0ad0cdab
oscarkann <oscarkang97@gmail.com>
2023-02-22 18:29:14 -0600

    js10, jest, babel

---
+++ JS/js10/ProyectoTest/calculadora/index.js
@@ -0,0 +1,35 @@
+
+//BABEL
+module.exports.indexTest = {
+    suma(a,b){
+        return a+b;
+    },
+    resta(a,b){
+        return a-b;
+    },
+    multi(a,b){
+        return a*b;
+    },
+    divi(a,b){
+        return a/b;
+    }
+}
+
+//NORMAL
+/*
+module.exports = {
+
+        suma (a, b){
+            return a+b;
+        },
+        resta (a, b){
+            return a-b;
+        },
+        multi (a, b){
+            return a*b;
+        },
+        divi (a, b){
+            return a/b;
+        }
+}
+*/
