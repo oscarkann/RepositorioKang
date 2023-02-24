68c4a67e958c6852d86e935fda90d3c961b2739d
oscarkann <oscarkang97@gmail.com>
2023-02-24 09:52:22 -0600

    Intro JAVA

---
+++ JAVA/Java-01/Java-Terminal/HolaMundo.java
@@ -0,0 +1,10 @@
+import Saludo;
+class HolaMundo{
+	public static void main (String [] ch23OscarKang){
+		//System.out.println("Hola mundo desde Java...");
+		
+		Saludo objSaludo;
+		objSaludo = new Saludo();
+		objSaludo.saludar();
+	}
+}
